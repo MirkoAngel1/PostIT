ae7f406c6430c040440b2ae1d9fdc80379dab64f
FabrizioMore <morefabrizio52@gmail.com>
2020-09-30 18:17:48 -0300

    Arreglado lo del login xd

--- static/js/login.js
+++ static/js/login.js
@@ -1,21 +1,17 @@
 var x = document.getElementById("login");
 var y = document.getElementById("register");
-var z = document.getElementById("toogle-btn");
-
-var submit_btn_register = document.getElementById("submit-btn-register");
-
+var z = document.getElementById("btn");
+var box_height = document.getElementsById("form-box");
 
 function register() {
   x.style.left = "-400px";
   y.style.left = "50px";
-  z.style.left = "140px";
-  z.style.borderRadius = "0px 30px 30px 0px";
-  submit_btn_register.style.marginTop = "30px";
-  
+  z.style.left = "130px";
+  box_height = "520px";
 }
 function login() {
   x.style.left = "50px";
   y.style.left = "450px";
   z.style.left = "0px";
-  z.style.borderRadius = "30px 0px 0px 30px";
+  box_height = "480px";
 }
