3a0ebfb2456cfcd4a8747a5c413cababb18b7b65
FabrizioMore <morefabrizio52@gmail.com>
2020-09-30 13:16:30 -0300

    Agrego login/register actualizado

---
+++ apps/login/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.db import models
+from django.contrib.auth.forms import User
+
+
+class changeEmailForm(forms.Form):
+
+    email = forms.EmailField(max_length=60, widget=forms.EmailInput(attrs={
+        "class": "input-field", "value": "", "placeholder": "Current Email"
+    }))
+    email2 = forms.EmailField(max_length=60, widget=forms.EmailInput(attrs={
+        "class": "input-field", "placeholder": "New Email"
+    }))
+    email3 = forms.EmailField(max_length=60, widget=forms.EmailInput(attrs={
+        "class": "input-field", "placeholder": "Repeat New Email"
+    }))
+
+    class Meta():
+        model = User
+        filter = ["email"]
+
+
+class changePassForm(forms.Form):
+    password = forms.CharField(max_length=60, widget=forms.PasswordInput(attrs={
+        "class": "input-field", "placeholder": "Current Password"
+    }))
+    password2 = forms.CharField(max_length=60, widget=forms.PasswordInput(attrs={
+        "class": "input-field", "placeholder": "New Password"
+    }))
+    password3 = forms.CharField(max_length=60, widget=forms.PasswordInput(attrs={
+        "class": "input-field", "placeholder": "Repeat New Password"
+    }))
+
+    class Meta():
+        model = User
+        filter = ['password']
+
+
+class changeUsernameForm(forms.Form):
+    username = forms.CharField(max_length=60, widget=forms.TextInput(
+        attrs={"class": "input-field", "placeholder": "Current Username"}))
+    username2 = forms.CharField(max_length=60, widget=forms.TextInput(
+        attrs={"class": "input-field", "placeholder": "New Username"}))
+    username3 = forms.CharField(max_length=60, widget=forms.TextInput(
+        attrs={"class": "input-field", "placeholder": "Repeat New Username"}))
+
+    class Meta():
+        model = User
+        filter = ['username']
