397a50e4529718e1bbf215182da147652f0eaa58
lezcano65 <lezcano65@hotmail.com>
2020-10-02 19:22:58 -0300

    aca esta

--- static/js/login.js
+++ static/js/login.js
@@ -7,14 +7,14 @@ var submit_btn_register = document.getElementById("submit-btn-register");
 
 function register() {
   x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "140px";
+  y.style.left = "90px";
+  z.style.left = "50%";
   z.style.borderRadius = "0px 30px 30px 0px";
   submit_btn_register.style.marginTop = "30px";
   
 }
 function login() {
-  x.style.left = "50px";
+  x.style.left = "90px";
   y.style.left = "450px";
   z.style.left = "0px";
   z.style.borderRadius = "30px 0px 0px 30px";
